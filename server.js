c16dfa2767d4b3bdd3a10d35c0e9e117773d1e94
s2na <59468535+whaizn@users.noreply.github.com>
2021-03-23 23:58:03 +0900

    first test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express'); // express 모듈 불러오기
 const app = express();  // express 객체 생성
 const path = require('path');   // path 모듈 불러오기
 const cors = require('cors');
-const port = process.env.PORT || 3001;  // 기본 포트를 app 객체에 설정
+const port = process.env.PORT || 3000;  // 기본 포트를 app 객체에 설정
 
 app.use(cors());
 app.use("/api", require("./routes"));
@@ -10,12 +10,15 @@ app.use('/api/data', function(req, res) {   // 미들웨어 함수를 특정 경
     res.json({ greeting: 'Hello World' });
 });
 
+/*
 app.use(express.static(path.join(__dirname, 'client/build')));  // 리액트 정적 파일 제공    (client폴더에서 npm run build)
 
 app.get('*', (req, res) => {    // 라우트 설정
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+*/
 
+app.get('/', (req, res) => res.send('Hello World'))
 
 
 app.listen(port, () => {
