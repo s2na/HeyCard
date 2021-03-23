f7893f18fdea909a074859ce78a58d19c800981b
s2na <59468535+whaizn@users.noreply.github.com>
2021-03-24 00:16:50 +0900

    build test

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.use('/api/data', function(req, res) {   // 미들웨어 함수를 특정 경
     res.json({ greeting: 'Hello World' });
 });
 
-
-app.use(express.static(path.join(__dirname, 'client/build')));  // 리액트 정적 파일 제공    (client폴더에서 npm run build)
+app.use(express.static(path.join(__dirname, './client/build')));  // 리액트 정적 파일 제공    (client폴더에서 npm run build)
 
 app.get('*', (req, res) => {    // 라우트 설정
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 
-app.get('/', (req, res) => res.send('Hello World'))
+
+//app.get('/', (req, res) => res.send('Hello World'))
 
 
 app.listen(port, () => {
