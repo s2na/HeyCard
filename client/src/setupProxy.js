85fbac06d44af9afc0b9c6bce018d37cf156dcb9
s2na <59468535+whaizn@users.noreply.github.com>
2021-04-17 00:41:10 +0900

    make it better

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app){
   app.use(
       createProxyMiddleware('/api', {
-          target: 'http://localhost:3001',
+          //target: 'http://localhost:3001',
+          target: 'https://heycard.herokuapp.com',
           changeOrigin: true
       })
   )
