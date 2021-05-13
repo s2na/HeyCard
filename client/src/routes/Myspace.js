0f4ca7b6575ebd3e54cb7828e972ec7fe123fc0c
s2na <59468535+whaizn@users.noreply.github.com>
2021-05-13 23:58:57 +0900

    make it better

--- client/src/routes/Myspace.js
+++ client/src/routes/Myspace.js
@@ -239,8 +239,8 @@ function Myspace({ usertoken, usermail }) {
         },
       });
 
-      console.log(JSON.parse(JSON.stringify(result)));
-      if (result >= 1) {
+      console.log(JSON.parse(JSON.stringify(result)).data);
+      if (JSON.parse(JSON.stringify(result)).data >= 1) {
         //명함이름이 중복되면 result가 1이기에
         setDuplicated(false); //duplicated변수는 명함이름이 중복되면 false를 가진다.
       }
